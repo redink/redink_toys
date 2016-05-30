c32befb9653a1a493613184740955b71b7f97cc9
redink <redink@ptthink.com>
2016-05-30 23:06:10 +0800

    add floor/ceiling binary search tree

--- code_for_fun/sort/sort_erlang/suanfa/c3.erl
+++ code_for_fun/sort/sort_erlang/suanfa/c3.erl
@@ -6,108 +6,156 @@
 -export([deletemin/1, deletemax/1]).
 -export([delete/2]).
 -export([prev/1, mid/1]).
+-export([floor/2, ceiling/2]).
 
 new() ->
-	{node, undefined, undefined, undefined, undefined}.
+    {node, undefined, undefined, undefined, undefined}.
 
 new(KVList) ->
-	lists:foldl(fun({Key, Value}, Acc) -> insert(Acc, {Key, Value}) end, new(), KVList).
+    lists:foldl(fun({Key, Value}, Acc) -> insert(Acc, {Key, Value}) end, new(), KVList).
 
 insert({node, undefined, undefined, undefined, undefined}, {Key, Value}) ->
-	{node, Key, undefined, undefined, Value};
+    {node, Key, undefined, undefined, Value};
 insert({node, Root, undefined, undefined, RootValue}, {Key, Value}) when Key < Root ->
-	{node, Root, {node, Key, undefined, undefined, Value}, undefined, RootValue};
+    {node, Root, {node, Key, undefined, undefined, Value}, undefined, RootValue};
 insert({node, Root, undefined, undefined, RootValue}, {Key, Value}) when Key > Root ->
-	{node, Root, undefined, {node, Key, undefined, undefined, Value}, RootValue};
+    {node, Root, undefined, {node, Key, undefined, undefined, Value}, RootValue};
 insert(undefined, {Key, Value}) ->
-	{node, Key, undefined, undefined, Value};
+    {node, Key, undefined, undefined, Value};
 
 insert({node, Root, Left, Right, _}, {Key, Value}) when Key == Root ->
-	{node, Root, Left, Right, Value};
+    {node, Root, Left, Right, Value};
 insert({node, Root, Left, Right, RootValue}, {Key, Value}) when Key < Root ->
-	{node, Root, insert(Left, {Key, Value}), Right, RootValue};
+    {node, Root, insert(Left, {Key, Value}), Right, RootValue};
 insert({node, Root, Left, Right, RootValue}, {Key, Value}) when Key > Root ->
-	{node, Root, Left, insert(Right, {Key, Value}), RootValue}.
+    {node, Root, Left, insert(Right, {Key, Value}), RootValue}.
 
 
 look(undefined, _) ->
-	not_found;
+    not_found;
 look({node, Root, _, _, RootValue}, Root) ->
-	RootValue;
+    RootValue;
 look({node, Root, Left, _, _}, Key) when Key < Root ->
-	look(Left, Key);
+    look(Left, Key);
 look({node, Root, _, Right, _}, Key) when Key > Root ->
-	look(Right, Key).
+    look(Right, Key).
 
 min({node, Root, undefined, _, _}) ->
-	Root;
+    Root;
 min({node, _, Left, _, _}) ->
-	min(Left).
+    min(Left).
 
 max({node, Root, _, undefined, _}) ->
-	Root;
+    Root;
 max({node, _, _, Right, _}) ->
-	max(Right).
+    max(Right).
 
 rank(undefined, _Key) ->
-	0;
+    0;
 rank({node, Root, Left, _, _}, Root) ->
-	1 + rank(Left);
+    1 + rank(Left);
 rank({node, Root, Left, _, _}, Key) when Key < Root ->
-	rank(Left, Key);
+    rank(Left, Key);
 rank({node, Root, Left, Right, _}, Key) when Key > Root ->
-	1 + rank(Left) + rank(Right, Key).
+    1 + rank(Left) + rank(Right, Key).
 
 
 rank({node, undefined, undefined, undefined, undefined}) ->
-	0;
+    0;
 rank(undefined) ->
-	0;
+    0;
 rank({node, _Root, Left, Right, _}) ->
-	rank(Left) + rank(Right) + 1.
+    rank(Left) + rank(Right) + 1.
 
 
 deletemin({node, undefined, undefined, undefined, undefined} = Node) ->
-	Node;
+    Node;
 deletemin({node, _Root, undefined, Right, _}) ->
-	Right;
+    Right;
 deletemin({node, Root, {node, _Root0, undefined, Right0, _}, Right, RootValue}) ->
-	{node, Root, Right0, Right, RootValue};
+    {node, Root, Right0, Right, RootValue};
 deletemin({node, Root, Left, Right, RootValue}) ->
-	{node, Root, deletemin(Left), Right, RootValue}.
+    {node, Root, deletemin(Left), Right, RootValue}.
 
 deletemax({node, undefined, undefined, undefined, undefined} = Node) ->
-	Node;
+    Node;
 deletemax({node, _Root, Left, undefined, _}) ->
-	Left;
+    Left;
 deletemax({node, Root, Left, {node, _Root0, Left0, undefined, _}, RootValue}) ->
-	{node, Root, Left, Left0, RootValue};
+    {node, Root, Left, Left0, RootValue};
 deletemax({node, Root, Left, Right, RootValue}) ->
-	{node, Root, Left, deletemax(Right), RootValue}.
+    {node, Root, Left, deletemax(Right), RootValue}.
 
 
 delete({node, Root, {node, Key, undefined, undefined, _}, Right, RootValue}, Key) ->
-	{node, Root, undefined, Right, RootValue};
+    {node, Root, undefined, Right, RootValue};
 delete({node, Root, Left, {node, Key, undefined, undefined, _}, RootValue}, Key) ->
-	{node, Root, Left, undefined, RootValue};
+    {node, Root, Left, undefined, RootValue};
 delete({node, Root, Left, undefined, _}, Root) ->
-	Left;
+    Left;
 delete({node, Root, Left, Right, _RootValue}, Root) ->
-	{NewRoot, NewRootValue} = get_root_right(Right),
-	{node, NewRoot, Left, deletemin(Right), NewRootValue};
+    {NewRoot, NewRootValue} = get_root_right(Right),
+    {node, NewRoot, Left, deletemin(Right), NewRootValue};
 delete({node, Root, undefined, Right, RootValue}, Key) when Key < Root ->
-	{node, Root, undefined, Right, RootValue};
+    {node, Root, undefined, Right, RootValue};
 delete({node, Root, Left, Right, RootValue}, Key) when Key < Root ->
-	{node, Root, delete(Left, Key), Right, RootValue};
+    {node, Root, delete(Left, Key), Right, RootValue};
 delete({node, Root, Left, undefined, RootValue}, Key) when Key > Root ->
-	{node, Root, Left, undefined, RootValue};
+    {node, Root, Left, undefined, RootValue};
 delete({node, Root, Left, Right, RootValue}, Key) when Key > Root ->
-	{node, Root, Left, delete(Right, Key), RootValue}.
+    {node, Root, Left, delete(Right, Key), RootValue}.
 
 get_root_right({node, Root, undefined, _Right, RootValue}) ->
-	{Root, RootValue};
+    {Root, RootValue};
 get_root_right({node, _Root, Left, _, _RootValue}) ->
-	get_root_right(Left).
+    get_root_right(Left).
+
+%%%%%%%%%%%%%%%%%%%%%
+floor({node, undefined, undefined, undefined, undefined}, _) ->
+    null;
+%% only one node
+floor({node, Root, undefined, undefined, RootValue}, _) ->
+    {Root, RootValue};
+%% root == key
+floor({node, Root, _, _, RootValue}, Root) ->
+    {Root, RootValue};
+%% left is undefined
+floor({node, Root, undefined, _, RootValue}, Key) when Key < Root ->
+    {Root, RootValue};
+floor({node, Root, Left, _, _RootValue}, Key) when Key < Root ->
+    floor(Left, Key);
+%% right is undefined
+floor({node, Root, _, undefined, RootValue}, Key) when Key > Root ->
+    {Root, RootValue};
+floor({node, Root, _, Right, RootValue}, Key) when Key > Root ->
+    case min(Right) =< Key of
+        false ->
+            {Root, RootValue};
+        true ->
+            floor(Right, Key)
+    end.
+
+ceiling({node, undefined, undefined, undefined, undefined}, _) ->
+    null;
+ceiling({node, Root, undefined, undefined, RootValue}, _) ->
+    {Root, RootValue};
+ceiling({node, Root, _, _, RootValue}, Root) ->
+    {Root, RootValue};
+%% right is undefined
+ceiling({node, Root, _, undefined, RootValue}, Key) when Key > Root ->
+    {Root, RootValue};
+ceiling({node, Root, _, Right, _RootValue}, Key) when Key > Root ->
+    ceiling(Right, Key);
+%% left is undefined
+ceiling({node, Root, undefined, _, RootValue}, Key) when Key < Root ->
+    {Root, RootValue};
+ceiling({node, Root, Left, _, RootValue}, Key) when Key < Root ->
+    case max(Left) >= Key of
+        false ->
+            {Root, RootValue};
+        true ->
+            ceiling(Left, Key)
+    end.
 
 prev({node, Value, undefined, undefined, _}) ->
     [Value];
@@ -126,3 +174,4 @@ mid({node, Value, undefined, Right, _}) ->
     [Value | mid(Right)];
 mid({node, Value, Left, Right, _}) ->
     mid(Left) ++ [Value] ++ mid(Right).
+
