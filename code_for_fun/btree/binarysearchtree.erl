2bf1f101a462e67a39bbdacd3340e4060277ce6e
redink <redink@ptthink.com>
2016-06-13 00:16:22 +0800

    delete unuse comment

--- code_for_fun/btree/binarysearchtree.erl
+++ code_for_fun/btree/binarysearchtree.erl
@@ -19,7 +19,7 @@
 
 -export([test/0]).
 
--define(undef, undefnied).
+-define(undef, undefined).
 -define(empty_tree,
         {node, ?undef, ?undef, ?undef, ?undef}).
 -define(only_root(Root, RootValue),
@@ -36,40 +36,27 @@ new(KVList) ->
     lists:foldl(fun({Key, Value}, Acc) -> insert(Acc, {Key, Value}) end,
                 new(), KVList).
 
-%% empty
 insert(?empty_tree, {Key, Value}) ->
     ?only_root(Key, Value);
-%% equal root
 insert({node, Root, Left, Right, _}, {Root, Value}) ->
     {node, Root, Left, Right, Value};
-%% < root
-%% left is ?undef
 insert(?left_undefined(Root, Right, RootValue), {Key, Value}) when Key < Root ->
-% insert({node, Root, ?undef, Right, RootValue}, {Key, Value}) when Key < Root ->
     {node, Root, ?only_root(Key, Value), Right, RootValue};
 insert({node, Root, Left, Right, RootValue}, {Key, Value}) when Key < Root ->
     {node, Root, insert(Left, {Key, Value}), Right, RootValue};
-%% > root
-%% right is ?undef
 insert(?right_undefined(Root, Left, RootValue), {Key, Value}) when Key > Root ->
-% insert({node, Root, Left, ?undef, RootValue}, {Key, Value}) when Key > Root ->
     {node, Root, Left, ?only_root(Key, Value), RootValue};
 insert({node, Root, Left, Right, RootValue}, {Key, Value}) when Key > Root ->
     {node, Root, Left, insert(Right, {Key, Value}), RootValue}.
 
-
-%% empty
 look(?empty_tree, _) ->
     [];
-%% == root
 look({node, Root, _, _, RootValue}, Root) ->
     [{Root, RootValue}];
-%% < root
 look(?left_undefined(Root, _, _), Key) when Key < Root ->
     [];
 look({node, Root, Left, _, _}, Key) when Key < Root ->
     look(Left, Key);
-%% > root
 look(?right_undefined(Root, _, _), Key) when Key > Root ->
     [];
 look({node, Root, _, Right, _}, Key) when Key > Root ->
@@ -85,22 +72,18 @@ max(?right_undefined(Root, _, _)) ->
 max({node, _, _, Right, _}) ->
     max(Right).
 
-%% empty
 rank(?empty_tree, _) ->
     0;
 rank(?undef, _) ->
     0;
-%% left is empty
 rank(?left_undefined(Root, _, _), Root) ->
     1;
 rank({node, Root, Left, _, _}, Root) ->
     1 + rank(Left);
-%% < root
 rank(?left_undefined(Root, _, _), Key) when Key < Root ->
     0;
 rank({node, Root, Left, _, _}, Key) when Key < Root ->
     rank(Left, Key);
-%% > root
 rank(?right_undefined(Root, Left, _), Key) when Key > Root ->
     1 + rank(Left, Key);
 rank({node, Root, Left, Right, _}, Key) when Key > Root ->
@@ -113,30 +96,26 @@ rank(?empty_tree) ->
 rank({node, _, Left, Right, _}) ->
     1 + rank(Left) + rank(Right).
 
-%% empty
 deletemin(?empty_tree = T) ->
     T;
-%% left is empty
 deletemin(?left_undefined(_, Right, _)) ->
     Right;
-%% left's left is empty
+%% left's left is ?undef
 deletemin({node, Root, ?left_undefined(_, LeftRight, _), Right, RootValue}) ->
     {node, Root, LeftRight, Right, RootValue};
 deletemin({node, Root, Left, Right, RootValue}) ->
     {node, Root, deletemin(Left), Right, RootValue}.
 
-%% empty
 deletemax(?empty_tree = T) ->
     T;
-%% right is empty
 deletemax(?right_undefined(_, Left, _)) ->
     Left;
+%% right's right is ?undef
 deletemax({node, Root, Left, ?right_undefined(_, RightLeft, _), RootValue}) ->
     {node, Root, Left, RightLeft, RootValue};
 deletemax({node, Root, Left, Right, RootValue}) ->
     {node, Root, Left, deletemax(Right), RootValue}.
 
-%% empty
 delete(?empty_tree = T, _) ->
     T;
 delete(?undef, _) ->
@@ -165,18 +144,14 @@ get_min({node, _, Left, _, _}) ->
 
 floor(?empty_tree, _) ->
     null;
-%% only one node
 floor(?only_root(Root, RootValue), _) ->
     {Root, RootValue};
-%% root == key
 floor({node, Root, _, _, RootValue}, Root) ->
     {Root, RootValue};
-%% left is ?undef
 floor(?left_undefined(Root, _, RootValue), Key) when Key < Root ->
     {Root, RootValue};
 floor({node, Root, Left, _, _RootValue}, Key) when Key < Root ->
     floor(Left, Key);
-%% right is ?undef
 floor(?right_undefined(Root, _, RootValue), Key) when Key > Root ->
     {Root, RootValue};
 floor({node, Root, _, Right, RootValue}, Key) when Key > Root ->
@@ -193,12 +168,10 @@ ceiling(?only_root(Root, RootValue), _) ->
     {Root, RootValue};
 ceiling({node, Root, _, _, RootValue}, Root) ->
     {Root, RootValue};
-%% right is ?undef
 ceiling(?right_undefined(Root, _, RootValue), Key) when Key > Root ->
     {Root, RootValue};
 ceiling({node, Root, _, Right, _RootValue}, Key) when Key > Root ->
     ceiling(Right, Key);
-%% left is ?undef
 ceiling(?left_undefined(Root, _, RootValue), Key) when Key < Root ->
     {Root, RootValue};
 ceiling({node, Root, Left, _, RootValue}, Key) when Key < Root ->
